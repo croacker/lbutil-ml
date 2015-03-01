4b731a1bbcd6a6e10c388377ac008a54bfcc20cb
rekcaorclaimg <croacker@mail.ru>
2015-03-02 00:08:46 +0300

    Process write metadata

---
+++ src/main/java/ru/croacker/lbutil/database/CommonResultSetTableConvertor.java
@@ -0,0 +1,39 @@
+package ru.croacker.lbutil.database;
+
+import org.springframework.stereotype.Service;
+import ru.croacker.lbutil.database.convertor.Convertor;
+import ru.croacker.lbutil.database.convertor.ResultSetConvertor;
+import ru.croacker.lbutil.database.metadata.MlTable;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ */
+@Service
+public class CommonResultSetTableConvertor extends ResultSetConvertor implements Convertor<ResultSet, MlTable> {
+
+  @Override
+  public MlTable toMetadata(ResultSet resultSet) {
+    MlTable mlTable = new MlTable();
+    mlTable.setId(getLong(resultSet, "id"));
+    mlTable.setTableName(getString(resultSet, "tableName"));
+    mlTable.setEntityName(getString(resultSet, "entityName"));
+    mlTable.setIsSystem(getBoolean(resultSet, "isSystem"));
+    mlTable.setJavaClass(getString(resultSet, "javaClass"));
+    mlTable.setHasHistory(getBoolean(resultSet, "hasHistory"));
+    mlTable.setDescription(getString(resultSet, "description"));
+    mlTable.setParent(getLong(resultSet, "parent"));
+    mlTable.setIsCacheable(getBoolean(resultSet, "isCacheable"));
+    mlTable.setTitleFormat(getString(resultSet, "titleFormat"));
+    mlTable.setHandlerClassName(getString(resultSet, "handlerClassName"));
+    mlTable.setReplicationHandlerClassName(getString(resultSet, "replicationHandlerClassName"));
+    return mlTable;
+  }
+
+  @Override
+  public ResultSet toTable(MlTable metadata) {
+    return null;
+  }
+
+}
